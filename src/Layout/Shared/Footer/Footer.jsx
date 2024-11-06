5684630116bc99b534a465883da7726bbad1653c
shsobur <shsoburhossen951@gmail.com>
2024-11-06 00:49:10 -0600

    Initial commit

---
+++ src/Layout/Shared/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return(
+    <>
+      <div className="main_footer_container">
+        <h2>Hello ztap, this Footer section</h2>
+      </div>
+    </>
+  )
+}
+
+export default Footer;
---
+++ src/Layout/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="main_bavbar_container">
+        <h2>Hello Ztap, this is -Navbar- Section</h2>
+      </div>
+    </>
+  )
+}
+
+export default Navbar;
