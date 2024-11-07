bc4fe3ba7de6a01b7ec0ef946c7f40ee69f3cd60
shsobur <shsoburhossen951@gmail.com>
2024-11-07 03:00:45 -0600

    Navebar for desktop design completed

--- src/Layout/Shared/Footer/Footer.jsx
+++ src/Layout/Shared/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import "./Footer.css";
 const Footer = () => {
   return(
     <>
-      <div className="main_footer_container">
-        <h2>Hello ztap, this Footer section</h2>
+      <div>
+        
       </div>
     </>
   )
--- src/Layout/Shared/Navbar/Navbar.jsx
+++ src/Layout/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,57 @@
-import "./Navbar.css"
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import { LuUserCircle } from "react-icons/lu";
+import { BsCartPlusFill } from "react-icons/bs";
+import logo from "../../../assets/logo.png";
 
 const Navbar = () => {
   return (
     <>
-      <div className="main_bavbar_container">
-        <h2>Hello Ztap, this is -Navbar- Section</h2>
+      <div className="main_navbar_outer_container">
+        <div className="main_navbar_inner_container">
+          <div className="navbar_image_outer_container">
+            <img src={logo} alt="logo" />
+          </div>
+
+          <div className="main_navbar_navigation_container">
+            <div className="main_navbar_navigate_route_container">
+              <ul>
+                <li>
+                  <NavLink>
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink>
+                    Shop
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink>
+                    Contact us
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink>
+                    Dashboard
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+
+            <div className="main_navbar_user_info_container">
+              <div className="navbar_cart_info_container">
+                <h3><BsCartPlusFill /></h3>
+                <span>99</span>
+              </div>
+              <h3><LuUserCircle /></h3>
+            </div>
+          </div>
+
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Navbar;
