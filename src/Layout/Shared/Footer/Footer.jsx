6114b710efd8dc8655a45a695e0b75189c22b793
shsobur <shsoburhossen951@gmail.com>
2024-11-10 08:04:03 -0600

    Footer design for small mobile completed

--- src/Layout/Shared/Footer/Footer.jsx
+++ src/Layout/Shared/Footer/Footer.jsx
@@ -11,24 +11,32 @@ const Footer = () => {
     <>
       <div className="main_container">
         <div className="main_footer_outer_container">
-
           <div className="footer_upper_section_container">
-
             <div className="first_column_container">
               <div className="footer_image_container">
                 <img src={logo} alt="logo" />
               </div>
-              <p>We have clothes that suits your style and which you’re proud to wear. From women to men.</p>
+              <p>
+                We have clothes that suits your style and which you’re proud to
+                wear. From women to men.
+              </p>
               <div className="footer_social_icons">
-                <h3><CiFacebook /></h3>
-                <h3><CiInstagram /></h3>
-                <h3><PiGithubLogoDuotone /></h3>
-                <h3><CiTwitter /></h3>
+                <h3>
+                  <CiFacebook />
+                </h3>
+                <h3>
+                  <CiInstagram />
+                </h3>
+                <h3>
+                  <PiGithubLogoDuotone />
+                </h3>
+                <h3>
+                  <CiTwitter />
+                </h3>
               </div>
             </div>
 
             <div className="second_patent_column_container">
-              
               <div className="second_chield_section_container">
                 <h3>Company</h3>
                 <p>About</p>
@@ -60,34 +68,35 @@ const Footer = () => {
                 <p>How to - Blog</p>
                 <p>Youtube Playlist</p>
               </div>
-
             </div>
-
           </div>
 
           <div className="footer_down_section_container">
-              <h2>Ztap © 2001-{currentYear}, All Rights Reserved</h2>
-              <div className="footer_card_image_container">
-                <img src={card} alt="image" />
-              </div>
+            <h2>Ztap © 2001-{currentYear}, All Rights Reserved</h2>
+            <div className="footer_card_image_container">
+              <img src={card} alt="image" />
+            </div>
           </div>
 
           <div className="main_news_letter_section_container">
-          <div className="news_letter_inner_container">
-
-            <div>
-              <h2>STAY UPTO DATE ABOUT<br/> OUR LATEST OFFERS</h2>
+            <div className="news_letter_inner_container">
+              <div>
+                <h2>
+                  STAY UPTO DATE ABOUT
+                  <br /> OUR LATEST OFFERS
+                </h2>
+              </div>
+              <div className="news_letter_input_section_container">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email address"
+                />
+                <button>Subscribe</button>
+              </div>
             </div>
-            <div className="news_letter_input_section_container">
-              <input type="email" name="email" placeholder="Enter your email address" />
-              <button>Subscribe to Newsletter</button>
-            </div>
-
           </div>
         </div>
-
-        </div>
-
       </div>
     </>
   );
