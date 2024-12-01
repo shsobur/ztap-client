8ebb253b1b979191fe1029bb4c3c49a339b9ed3d
shsobur <shsoburhossen951@gmail.com>
2024-12-01 11:06:32 +0600

    filter section design completed

--- src/Layout/Pages/ShopPage/ShopPageLayout/ShopPageLayout.jsx
+++ src/Layout/Pages/ShopPage/ShopPageLayout/ShopPageLayout.jsx
@@ -1,6 +1,6 @@
-import { IoIosMenu } from "react-icons/io";
 import "./ShopPageLayout.css";
 import { LuFilter } from "react-icons/lu";
+import { IoIosMenu } from "react-icons/io";
 
 const ShopPageLayout = () => {
   return (
@@ -48,10 +48,39 @@ const ShopPageLayout = () => {
                 </select>
               </div>
 
+              <div className="size_filter_container">
+                <h2>Size_</h2>
+                <div className="size_categotu_content_container">
+                  <h3 value="small">Small</h3>
+                  <h3 value="medium">Medium</h3>
+                  <h3 value="xl">xl</h3>
+                  <h3 value="2xl">2xl</h3>
+                </div>
+              </div>
+
+              <div className="color_filter_container">
+                <h2>Color_</h2>
+                <div className="color_categotu_content_container">
+                  <div id="color_1"></div>
+                  <div id="color_2"></div>
+                  <div id="color_3"></div>
+                  <div id="color_4"></div>
+                  <div id="color_5"></div>
+                  <div id="color_6"></div>
+                  <div id="color_7"></div>
+                  <div id="color_8"></div>
+                  <div id="color_9"></div>
+                </div>
+              </div>
+
             </div>
           </div>
 
-          <div className="shop_item_section_container">Item</div>
+          <div className="shop_item_section_container">
+            <div className="search_bar_container">
+              
+            </div>
+          </div>
 
         </div>
       </div>
