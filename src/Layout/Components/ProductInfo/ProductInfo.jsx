9139606e3341f4f3f4343d07f5698266f497f0a0
shsobur <shsoburhossen951@gmail.com>
2025-02-01 11:00:17 +0600

    Product details page resopnsive style completed

--- src/Layout/Components/ProductInfo/ProductInfo.jsx
+++ src/Layout/Components/ProductInfo/ProductInfo.jsx
@@ -1,14 +1,13 @@
 import "./ProductInfo.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { FiMinus, FiPlus } from "react-icons/fi";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const ProductInfo = ({plus, minus, quantity}) => {
+const ProductInfo = ({ plus, minus, quantity }) => {
   return (
     <>
       <div className="product_info_inner_container">
-
         <div className="product_image_main_container">
           <Carousel
             showArrows={false}
@@ -31,7 +30,6 @@ const ProductInfo = ({plus, minus, quantity}) => {
 
         <div className="product_card_details_main_contaenr">
           <div className="first_details_container">
-
             <h2>One Life Graphic T-shirt</h2>
             <h3>⭐⭐⭐⭐ 4/5</h3>
 
@@ -41,10 +39,14 @@ const ProductInfo = ({plus, minus, quantity}) => {
               <h4>-43%</h4>
             </div>
 
-            <p>This graphic t-shirt which is perfect for any occasion. Crafted from a soft and breathable fabric, it offers superior comfort and style.</p>
+            <p>
+              This graphic t-shirt which is perfect for any occasion. Crafted
+              from a soft and breathable fabric, it offers superior comfort and
+              style.
+            </p>
 
             <div className="product_color_section">
-              <h2>Available Color-</h2>
+              <h2>Available Color_</h2>
               <ul>
                 <li>Black</li>
                 <li>Red</li>
@@ -54,7 +56,7 @@ const ProductInfo = ({plus, minus, quantity}) => {
             </div>
 
             <div className="product_size_section">
-              <h2>Available Size</h2>
+              <h2>Available Size_</h2>
               <ul>
                 <li>Small</li>
                 <li>Medium</li>
@@ -65,16 +67,20 @@ const ProductInfo = ({plus, minus, quantity}) => {
 
             <div className="product_buy_btn_container">
               <div className="product_count_container">
-                <button onClick={plus}><FiPlus /></button>
-                  <h3>{quantity}</h3>
-                <button onClick={minus}><FiMinus /></button>
+                <button onClick={plus}>
+                  <FiPlus />
+                </button>
+                <h3>{quantity}</h3>
+                <button onClick={minus}>
+                  <FiMinus />
+                </button>
               </div>
-              <button className="product_add_cart_btn_container">Add to Cart</button>
+              <button className="product_add_cart_btn_container">
+                Add to Cart
+              </button>
             </div>
-
           </div>
         </div>
-
       </div>
     </>
   );
@@ -83,7 +89,7 @@ const ProductInfo = ({plus, minus, quantity}) => {
 ProductInfo.propTypes = {
   plus: PropTypes.func,
   minus: PropTypes.func,
-  quantity: PropTypes.number
-}
+  quantity: PropTypes.number,
+};
 
 export default ProductInfo;
